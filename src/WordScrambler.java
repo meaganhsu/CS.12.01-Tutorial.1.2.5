03776716a4860f553c8cbafbcfdefeb5ddc53a83
meaganhsu <myth72@student.cis.edu.hk>
2024-09-06 16:46:39 +0800

    set

--- src/WordScrambler.java
+++ src/WordScrambler.java
@@ -7,6 +7,9 @@ public class WordScrambler {
     public String[] getScrambledWords() {
         return scrambledWords;
     }
+    public void setScrambledWords(String[] scrambledWords) {
+        this.scrambledWords = scrambledWords;
+    }
     private String recombine(String word1, String word2) {
         return word1.substring(0, word1.length()/2) + word2.substring(word2.length()/2);
     }
